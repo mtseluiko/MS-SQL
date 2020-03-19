b75f519558afa86f430a453c5d9ff99f9ad0a8ec
Kostya <kostantin.pilkevych@binary-studio.com>
2020-03-19 17:09:41 +0200

    fix relationships with missing tables

--- reverse_engineering/helpers/filterRelationships.js
+++ reverse_engineering/helpers/filterRelationships.js
@@ -1,8 +1,13 @@
+const doesCollectionsExist = collections => relationship => {
+	const parentCollection = collections.find(({ dbName, collectionName }) =>
+		relationship.dbName === dbName && collectionName === relationship.parentCollection );
+
+	const childCollection = collections.find(({ dbName, collectionName }) =>
+		relationship.childDbName === dbName && collectionName === relationship.childCollection );
+	return Boolean(parentCollection && childCollection);
+};
+
 const filterRelationships = (relationships, collections) =>
-	relationships.filter(relationship => Boolean(
-		collections.find(({ collectionName, dbName }) =>
-			dbName === relationship.dbName
-			&& (relationship.childCollection === collectionName || relationship.parentCollection === collectionName))
-	));
+	relationships.filter(doesCollectionsExist(collections));
 
 module.exports = filterRelationships;
